57d984aeaf58fd95a463bbf9c6bf97b84478bcac
dolsen0 <derek.olsen@gmail.com>
2022-08-25 10:26:47 -0400

    added save functionality

--- src/renderer/components/Photo.tsx
+++ src/renderer/components/Photo.tsx
@@ -1,7 +1,7 @@
 import {useState, useCallback} from 'react';
 import Cropper from 'react-easy-crop';
 import {Area} from 'react-easy-crop/types'
-import { readFile } from 'helpers/images';
+import { readFile, cropImageData } from 'helpers/images';
 
 
 export default function Photo(){
@@ -9,8 +9,12 @@ export default function Photo(){
   const [crop, setCrop] = useState({x: 0, y:0});
   const [zoom, setZoom] = useState(1);
   const [fileName, setFileName] = useState(null); //file address
+
   const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>()
-  const onCropComplete = useCallback((croppedArea: Area, croppedAreaPixels: Area) => {}, [])
+  const onCropComplete = useCallback((_croppedArea: Area, currentCroppedAreaPixels: Area) => {
+    setCroppedAreaPixels(currentCroppedAreaPixels);
+  }, []);
+
   const handleFileChange = async (e: any)=>{
     if(e.target.files && e.target.files.length){
       //this shows we receivee a file..
@@ -23,9 +27,15 @@ export default function Photo(){
     }
   }
 
-  const handleSave = () => {
+  const handleSave = async () => {
     //save cropped image
-
+    //first create cropped image data using a canvas...
+    const base64data = await cropImageData(imageSrc, croppedAreaPixels!)
+    .catch(console.error);
+    //create a new filename
+    const newFileName = fileName + '-cropped.png'
+    //then send those results to saveImage via upcRender event
+    window.electron.saveCroppedImage([newFileName, base64data]);
     //reset interface
     setImageSrc(null);
     setZoom(1);
